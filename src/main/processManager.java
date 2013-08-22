7fe0318ab5ecc8d0c09d2afaecf91999c2eb7d73
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-08-21 21:08:22 -0400

    Adding IO managers

--- src/main/processManager.java
+++ src/main/processManager.java
@@ -1,25 +1,37 @@
 package main;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import master.*;
+import slave.*;
+
 public class processManager {
 
 	/**
 	 * @param args
+	 * @throws IOException 
+	 * @throws UnknownHostException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		// TODO Auto-generated method stub
 		
 		if(args.length==2){
-			System.out.println("I am a slave");
+		
 			if(args[0].equals("-c")){
-				System.out.println("-c detected");
+				System.out.println("-c detected / hostname:"+args[1]);
+				System.out.println("I am a slave");
+				Slave.runSlave(args[1]);
 			}
 			else
 				System.out.println("Invalid Argument; use -c for slave mode");
 		}
 		else if(args.length>2 || args.length==1)
 			System.out.println("Error: usage processmanager / processmanager -c  <hostname>");
-		else
+		else{
 			System.out.println("I am a master");
+			Master.runMaster();
+		}
 
 	}
 
---
+++ src/master/Master.java
@@ -0,0 +1,20 @@
+package master;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Master {
+
+	public static void runMaster() throws IOException{
+		int port=8081;
+		ServerSocket servSocket= new ServerSocket(port);
+		System.out.println("Server listening on port:"+servSocket.getLocalPort());
+		while (true){
+			Socket socket=servSocket.accept();
+			System.out.println("Connection established to"+ socket.getInetAddress()+":"+socket.getPort());
+		}
+		
+	}
+
+}
