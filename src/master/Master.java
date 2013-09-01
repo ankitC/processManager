31885a7852b372ba14a2335472c8d3764cc43355
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-01 16:54:55 -0400

    Adding a master thread for accepting commands

--- src/master/Master.java
+++ src/master/Master.java
@@ -7,6 +7,11 @@ import java.net.Socket;
 public class Master {
 
 	public static void runMaster() throws IOException{
+		
+		MasterThread masterThread=new MasterThread();
+		Thread master=new Thread(masterThread);
+		master.run();
+		
 		int port=8081;
 		ServerSocket servSocket= new ServerSocket(port);
 		System.out.println("Server listening on port:"+servSocket.getLocalPort());
