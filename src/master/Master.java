2b35d4dd806f0f36a223d12d2ba64b02ccc4c8c7
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-02 01:10:50 -0400

    Adding the thread for Listener

--- src/master/Master.java
+++ src/master/Master.java
@@ -3,23 +3,39 @@ package master;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class Master {
+	
+	public static HashMap<Integer, Socket> workerToSocket=new HashMap<Integer,Socket>();
+	public static HashMap<Integer,Integer> pidToWorker=new HashMap<Integer,Integer>();
+	public static HashMap<Integer,String> pidToStatus=new HashMap<Integer,String>();
+	public static HashMap<Integer,String> pidToCommand=new HashMap<Integer, String>();
+	public static HashMap<Integer,MasterListener>workerToListner=new HashMap<Integer, MasterListener>();
+	
+	public static int serverPort;
+	public static int workerNumber=0;
+	
 
+	
 	public static void runMaster() throws IOException{
 		
-		MasterThread masterThread=new MasterThread();
+		MasterConsole masterThread=new MasterConsole();
 		Thread master=new Thread(masterThread);
-		master.run();
+		master.start();
 		
-		int port=8081;
+		int port=serverPort;
 		ServerSocket servSocket= new ServerSocket(port);
 		System.out.println("Server listening on port:"+servSocket.getLocalPort());
 		while (true){
 			Socket socket=servSocket.accept();
 			System.out.println("Connection established to"+ socket.getInetAddress()+":"+socket.getPort());
+			workerNumber++;
+			workerToSocket.put(workerNumber, socket);
+			MasterListener m=new MasterListener(socket, workerNumber);
+			workerToListner.put(workerNumber, m);
+			Thread listenerThread=new Thread(m);
+			listenerThread.start();
 		}
-		
 	}
-
 }
