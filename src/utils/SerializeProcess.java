73df5c69619f64681ae5632390d5127fdc5c114c
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-02 03:48:24 -0400

    Adding utility to serialize the process

---
+++ src/utils/SerializeProcess.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import migratableProcess.MigratableProcess;
+
+public class SerializeProcess {
+	
+	public static void serializeProcess(int pid, MigratableProcess task){
+		FileOutputStream file;
+		try {
+			file = new FileOutputStream("/afs/ece/usr/achheda/ds/workspace/lab1/"+Integer.toString(pid)+".ser");
+	
+		//	OutputStream outStrm=new OutputStream("/afs/ece/usr/achheda/ds/workspace/lab1/"+Integer.toString(taskID)+".ser");
+		ObjectOutputStream objectOutStrm=new ObjectOutputStream(file);
+		objectOutStrm.writeObject(task);
+		objectOutStrm.flush();
+		objectOutStrm.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
