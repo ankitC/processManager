34fc66adf0d4751de837e05e066d82be729c411c
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-07 17:21:41 -0400

    Adding classes for encode process and decode process

---
+++ src/Decode.java
@@ -0,0 +1,63 @@
+
+
+import java.io.IOException;
+
+import migratableProcess.MigratableProcess;
+
+import IO.TransactionalFileInputStream;
+import IO.TransactionalFileOutputStream;
+
+public class Decode implements MigratableProcess {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public TransactionalFileInputStream inFile;
+	public TransactionalFileOutputStream outFile;
+	public boolean suspending;
+	
+	public Decode(String[] arguments){
+		inFile=new TransactionalFileInputStream(arguments[1]);
+		outFile= new TransactionalFileOutputStream(arguments[2]);
+	}
+
+	public void run() {
+
+		suspending=false;
+		while(!suspending){
+			try {
+				int a=inFile.read();
+				int b=inFile.read();
+
+				if(a==-1||b==-1)
+					break;
+				char first=(char)a;
+				char second=(char)b;
+				
+				StringBuffer letter= new StringBuffer();
+				letter.append(first);
+				letter.append(second);
+				
+				int asciiLetter=Integer.parseInt(letter.toString(), 16) ;
+				outFile.write(asciiLetter);
+				
+				Thread.sleep(500);
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		suspending=false;
+	}
+
+	public void suspend() {
+		suspending=true;
+		while(suspending);
+	}
+
+
+}
---
+++ src/Encode.java
@@ -0,0 +1,74 @@
+
+
+import java.io.IOException;
+
+import migratableProcess.MigratableProcess;
+
+import IO.TransactionalFileInputStream;
+import IO.TransactionalFileOutputStream;
+
+public class Encode implements MigratableProcess{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public TransactionalFileInputStream inFile;
+	public TransactionalFileOutputStream outFile;
+	public boolean suspending;
+	public boolean canBeSerlialized;
+	
+	public Encode(String[] arguments){
+		inFile=new TransactionalFileInputStream(arguments[1]);
+		outFile= new TransactionalFileOutputStream(arguments[2]);
+	}
+
+	public void run() {
+
+		suspending=false;
+		canBeSerlialized=false;
+		while(!suspending){
+
+			int a;
+			try {
+				a = inFile.read();
+
+				if(a==-1)
+					break;
+				
+				String conv=new String(Integer.toHexString(a));
+				byte[] b;
+				b = conv.getBytes("US-ASCII");
+
+				for(int i=0; i<b.length;i++)
+					outFile.write((int)b[i]);
+				
+				Thread.sleep(500);
+
+
+			}catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		suspending=false;
+		canBeSerlialized=true;
+	}
+
+	public void suspend() {
+		// TODO Auto-generated method stub
+		suspending=true;
+		while(suspending){
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
