ec0f46208bcc4f8a0b2ebf311acaeead16889813
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-14 21:56:58 -0400

    Removing the worker port

--- src/common/Config.java
+++ src/common/Config.java
@@ -3,9 +3,6 @@ public class Config {
 
 	/*Port number for the server to listen on*/
 	public static int serverPort=8081;
-
-    /*Default worker port*/
-    public static int workerPort=9091;
 	
 	/*Directory where the processes will be serialzed to*/
 	public static String  serializeDirectory="/afs/ece.cmu.edu/usr/achheda/ds/processManager/";
