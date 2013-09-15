5fa08bd9094dd7d86994ee9a2e7f78003969c0ff
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-14 22:01:54 -0400

    Adding methods more verbose

--- src/common/Marshaller.java
+++ src/common/Marshaller.java
@@ -8,14 +8,17 @@ import migratableProcess.MigratableProcess;
 public class Marshaller {
 
     public static void serialize(MigratableProcess task){
+    	task.toString();
         FileOutputStream file;
         try {
+        	System.out.println("Serializing Process "+task.getPid());
             new File(Config.serializeDirectory+Integer.toString(task.getPid())+".ser").delete();
             file = new FileOutputStream(Config.serializeDirectory+Integer.toString(task.getPid())+".ser");
             ObjectOutputStream objectOutStrm = new ObjectOutputStream(file);
             objectOutStrm.writeObject(task);
             objectOutStrm.flush();
             objectOutStrm.close();
+            System.out.println("Done");
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -25,12 +28,15 @@ public class Marshaller {
     public static MigratableProcess deserialize(int pid){
         FileInputStream file;
         try {
+        	System.out.println("DeSerializing Process "+ pid);
             file = new FileInputStream(Config.serializeDirectory+Integer.toString(pid)+".ser");
             ObjectInputStream objectInStrm=new ObjectInputStream(file);
             Object o=objectInStrm.readObject();
             MigratableProcess m=(MigratableProcess)o;
             objectInStrm.close();
             new File(Config.serializeDirectory+Integer.toString(pid)+".ser").delete();
+            m.toString();
+            System.out.println("Done");
             return m;
         } catch (IOException e) {
             // TODO Auto-generated catch block
